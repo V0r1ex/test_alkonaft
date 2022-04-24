c04326e3ffd76603abd621e7d60b6e3725b0c469
Аркадий <102917564+Modest301@users.noreply.github.com>
2022-04-24 14:00:48 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -10,26 +10,16 @@ main_win.resize(400, 200)
 
 v_line = QVBoxLayout()
 h_line1 = QHBoxLayout()
-h_line2 = QHBoxLayout()
-h_line3 = QHBoxLayout()
+
 
 text1 = QLabel('Python')
-text2 = QLabel('PHP')
-text3 = QLabel('C')
-text4 = QLabel('C++')
-text5 = QLabel('C#')
-text6 = QLabel('GODOT')
 
-h_line1.addWidget(text1, alignment=Qt.AlignCenter)
-h_line2.addWidget(text2, alignment=Qt.AlignCenter)
-h_line3.addWidget(text3, alignment=Qt.AlignCenter)
-h_line2.addWidget(text4, alignment=Qt.AlignCenter)
-h_line1.addWidget(text5, alignment=Qt.AlignCenter)
-h_line3.addWidget(text6, alignment=Qt.AlignCenter)
+
+v_line.addWidget(text1, alignment=Qt.AlignCenter)
+
 
 v_line.addLayout(h_line1)
-v_line.addLayout(h_line2)
-v_line.addLayout(h_line3)
+
 
 main_win.setLayout(v_line)
 h_line1 = QHBoxLayout()
